c579ec265cd9824c99710a11f94638ea2b78945c
Yuke Wang <yuke_wang@seal-ampere.ece.ucsb.edu>
2021-06-29 06:02:27 +0000

    update with 2D profiling

--- 5_modeling_table.py
+++ 5_modeling_table.py
@@ -9,22 +9,23 @@ for fname in files:
         continue
     fname_li.append(fname)
 
-fname_li.sort(lambda x: (int(x.rstrip('.csv').split('_')[-3]), int(x.rstrip('.csv').split('_')[-1]) ) )
-print(fname_li)
+fname_li.sort(key=lambda x: (int(x.rstrip('.csv').split('_')[-3]), int(x.rstrip('.csv').split('_')[-1]) ) )
+# print(fname_li)
 
 output_dict = {}
 for fname in fname_li:
     fp = open(fname, "r")
     for line in fp:
-        time  = float(line.rstrip('\n').split(','))
+        time  = float(line.rstrip('\n').split(',')[1])
         break
     output_dict[fname] = time
 
 
-ldim = 6
+ldim = 5
 for i, fname in enumerate(fname_li):
     if i != 0 and i%ldim == 0:
         print()
-    else:
-        print(output_dict[fname]+",", end="")
+    print(str(output_dict[fname])+",", end="")
+
+print()
 
