085534a7ae02db88df4af0f34dd13abf8aee76e2
Yuke Wang <yuke_wang@seal-ampere.ece.ucsb.edu>
2021-07-09 04:47:27 +0000

    update with profiling script

--- run_profile.sh
+++ run_profile.sh
@@ -1,5 +1,10 @@
-./6_profile_uvm.py | tee profile_uvm.log
-./1_analysis_profile.py profile_uvm.log 9
+# ./6_profile_uvm.py | tee profile_uvm.log
+# ./1_analysis_profile.py profile_uvm.log 9
 
-./7_profile_mgg.py | tee profile_mgg.log
-./1_analysis_profile.py profile_mgg.log 9
+# ./7_profile_mgg.py | tee profile_mgg.log
+# ./1_analysis_profile.py profile_mgg.log 9
+
+
+# nsys profile --stats='true' --gpu-metrics-device=0 --cuda-um-gpu-page-faults='true' --cuda-um-cpu-page-faults='true' --show-output='true' \
+nsys profile --stats=true --cuda-um-gpu-page-faults=true --cuda-um-cpu-page-faults=true --show-output=true --export=json \
+build/uvm_profile dataset/cora.mtx 16 16
