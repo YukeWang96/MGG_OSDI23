b6c89c446069e4f4e890706054325a300f9c9fc6
Yuke Wang <yuke_wang@seal-ampere.ece.ucsb.edu>
2021-07-09 22:51:47 +0000

    update with pagefault duration

---
+++ 9_parse_page_fault_dur.py
@@ -0,0 +1,30 @@
+import json
+import os
+import sys
+
+files = os.listdir('.')
+
+flist = []
+for fname in files:
+    if '.json' in fname:
+        flist.append(fname)
+
+flist.sort()
+
+for fname in flist:
+
+    f = open(fname)
+
+    page_li = []
+    for line in f:
+        if 'CudaUvmGpuPageFaultEvent' in line:
+            page_li.append(line)
+
+    total_cnt = 0
+    for line in page_li:
+        test = json.loads(line)
+        start_ = test['CudaUvmGpuPageFaultEvent']['startNs']
+        end_ = test['CudaUvmGpuPageFaultEvent']['endNs']
+        total_cnt += int(int(end_) - int(start_))
+
+    print("{}, {}".format(fname, total_cnt))
