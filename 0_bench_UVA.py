6e9a7b022f33bbe5934d173623ac0de76590ee94
Yuke Wang <yuke_wang@seal-ampere.ece.ucsb.edu>
2021-07-12 01:04:14 +0000

    update with nsys profiling

--- 0_bench_UVA.py
+++ 0_bench_UVA.py
@@ -38,10 +38,10 @@ dataset = [
         # ( 'soc-BlogCatalog'	         , 128	  , 39),      
         # ( 'amazon0601'  	         , 96	  , 22), 
 
-        ( 'Reddit'                      , 602      	, 41),
+        # ( 'Reddit'                      , 602      	, 41),
         # ( 'enwiki-2013'	                , 100	    , 12),      
         # ( 'ogbn-products'	            , 100	    , 47),
-        # ( 'ogbn-proteins'		        , 8		    , 112),
+        ( 'ogbn-proteins'		        , 8		    , 112),
         # ( 'com-Orkut'				    , 128		, 128),
 
         # ( 'web-Google'				    , 128		, 128),
