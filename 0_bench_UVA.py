fd5ae8bccf5231fcc8716ad992a6a2bc2efcdb6f
Yuke Wang <yuke_wang@seal-ampere.ece.ucsb.edu>
2022-04-01 16:30:36 +0000

    update with UVM benchmark

--- 0_bench_UVA.py
+++ 0_bench_UVA.py
@@ -20,30 +20,31 @@ warpPerblock = 4
 # warpPerblock = int(sys.argv[1])
 
 dataset = [
-        ('citeseer'	        		, 3703	    , 6   ),  
-        ('cora' 	        		, 1433	    , 7   ),  
-        ('pubmed'	        		, 500	    , 3   ),      
-        ('ppi'	            		, 50	    , 121 ),   
+        # ('citeseer'	        		, 3703	    , 6   ),  
+        # ('cora' 	        		, 1433	    , 7   ),  
+        # ('pubmed'	        		, 500	    , 3   ),      
+        # ('ppi'	            		, 50	    , 121 ),   
         
-        ('PROTEINS'                  , 29       , 2) ,   
-        ('OVCAR-8H'                  , 66       , 2) , 
-        ('Yeast'                     , 74       , 2) ,
-        ('DD'                        , 89       , 2) ,
-        ('SW-620H'                   , 66       , 2) ,
+        # ('PROTEINS'                  , 29       , 2) ,   
+        # ('OVCAR-8H'                  , 66       , 2) , 
+        # ('Yeast'                     , 74       , 2) ,
+        # ('DD'                        , 89       , 2) ,
+        # ('SW-620H'                   , 66       , 2) ,
 
-        ( 'amazon0505'               , 96	  , 22),
-        ( 'artist'                   , 100	  , 12),
-        ( 'com-amazon'               , 96	  , 22),
-        ( 'soc-BlogCatalog'	     , 128	  , 39),      
-        ( 'amazon0601'  	     , 96	  , 22), 
+        # ( 'amazon0505'               , 96	  , 22),
+        # ( 'artist'                   , 100	  , 12),
+        # ( 'com-amazon'               , 96	  , 22),
+        # ( 'soc-BlogCatalog'	     , 128	  , 39),      
+        # ( 'amazon0601'  	     , 96	  , 22), 
 
-        ( 'Reddit'                      , 602       , 41),
-        ( 'enwiki-2013'	                , 100	    , 12),      
-        ( 'ogbn-products'	        , 100	    , 47),
-        ( 'ogbn-proteins'	        , 8	    , 112),
-        ( 'com-Orkut'	                , 128       , 128),
+        # ( 'Reddit'                      , 602       , 41),
+        # ( 'enwiki-2013'	                , 100	    , 12),      
+        # ( 'ogbn-products'	        , 100	    , 47),
+        # ( 'ogbn-proteins'	        , 8	    , 112),
+        # ( 'com-Orkut'	                , 128       , 128),
 
-        # ('paper100M'                  , 128       , 172)
+        ('paper100M'                  , 128       , 172),
+        ('mag240m'                  , 128       , 172)
 
         # ( 'web-Google'				    , 128		, 128),
         # ( 'wiki-Talk'				    , 128		, 128),
