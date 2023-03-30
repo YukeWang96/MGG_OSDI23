1837044a1cd1976ff8fffaabca32fb9b291b5a88
Yuke Wang <yuke_wang@ucsb.edu>
2023-03-30 03:10:48 +0000

    update with files

--- 6_MGG_DSE.py
+++ 6_MGG_DSE.py
@@ -8,11 +8,11 @@ for dist in [1,2,4,8,16]:
         os.system("./0_bench_MGG_DSE_dist_ps.py {0} {1}| tee MGG_dist_{0}_ps_{1}.log".format(dist, ps))
         os.system("./1_analysis_MGG.py MGG_dist_{0}_ps_{1}.log {2}".format(dist, ps, num_gpus))
         os.system("mv MGG_dist_{0}_ps_{1}.log logs/".format(dist, ps))
-os.system("./10_extract_search.py 6 > Reddit_8xA100_dist_ps.csv")
+os.system("./1_extract_search.py 6 > Reddit_8xA100_dist_ps.csv")
 
 for dist in [1,2,4,8,16]:
     for wpb in [1,2,4,8,16]:
         os.system("./0_bench_MGG_DSE_dist_wpb.py {0} {1}| tee MGG_dist_{0}_wpb_{1}.log".format(dist, wpb))
         os.system("./1_analysis_MGG.py MGG_dist_{0}_wpb_{1}.log {2}".format(dist, wpb, num_gpus))
         os.system("mv MGG_dist_{0}_wpb_{1}.log logs/".format(dist, wpb))
-os.system("./10_extract_search.py 5 > Reddit_8xA100_dist_wpb.csv")
+os.system("./1_extract_search.py 5 > Reddit_8xA100_dist_wpb.csv")
