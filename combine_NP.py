cff0ec836de7403276a31a2c1e32cf4410560b24
Yuke Wang <yuke_wang@ucsb.edu>
2023-04-02 21:00:20 +0000

    update with output results

--- combine_NP.py
+++ combine_NP.py
@@ -27,12 +27,20 @@ import csv
 # Read the first CSV file and get a column
 with open('MGG_8GPU_WO_NP.csv', 'r') as file1:
     reader = csv.reader(file1)
+    col0 = [row[0] for row in reader]
+
+with open('MGG_8GPU_WO_NP.csv', 'r') as file1:
+    reader = csv.reader(file1)
     col1 = [float(row[1]) for row in reader]
+
+print(col0)
+print(col1)
 
 # Read the second CSV file and get a column
 with open('MGG_8GPU_NP.csv', 'r') as file2:
     reader = csv.reader(file2)
     col2 = [float(row[1]) for row in reader]
+print(col2)
 
 # Compute the ratio of the values in the two columns
 ratio = [col1[i] / col2[i] for i in range(len(col1))]
@@ -40,6 +48,6 @@ ratio = [col1[i] / col2[i] for i in range(len(col1))]
 # Write the result, along with the original two columns, to a third CSV file
 with open('result.csv', 'w', newline='') as result_file:
     writer = csv.writer(result_file)
-    writer.writerow(['column_name_1', 'column_name_2', 'ratio_column'])
+    writer.writerow(['Dataset', 'MGG_WO_NP', 'MGG_W_NP', 'Speedup (x)'])
     for i in range(len(col1)):
-        writer.writerow([col1[i], col2[i], ratio[i]])
+        writer.writerow([col0[i].rstrip("_beg_pos"), col1[i], col2[i], "{:.3f}".format(ratio[i])])
