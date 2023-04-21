15818c8b04a774574e4613908c1ffdfca6447c3e
Yuke Wang <yuke_wang@ucsb.edu>
2023-04-21 22:00:39 +0000

    update with files

--- 0_mgg_build.sh
+++ 0_mgg_build.sh
@@ -1,11 +1,5 @@
 cd build/
 
-# make MGG_single_kernel
-# make MGG_basic
-# make MGG_np
-
-# make MGG_np_div_gin
-# make MGG_np_div
 # make MGG_np_div_kernel
 # make MGG_np_div_mem
 # make MGG_np_div_gin_mem
@@ -14,19 +8,23 @@ cd build/
 # make MGG_np_div_th
 # make MGG_np_div_blk
 
-# make uvm_gcn_2layer
-# make uvm_gin_5layer
-
-make uvm_gcn_2layer_mem
+# make uvm_gcn_2layer_mem
 # make uvm_gin_5layer_mem
 
 cd ..
 mkdir logs/
 mkdir csvs/
 
+# make MGG_single_kernel
+# make MGG_basic
+# make MGG_np
 
 
+# make MGG_np_div_gin
+# make MGG_np_div
 
+# make uvm_gcn_2layer
+# make uvm_gin_5layer
 
 # make MGG_np_pipeline
 # make unified_memory
