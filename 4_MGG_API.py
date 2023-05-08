ac727243ccf7469fe9f111655b5ab5368f2da767
Ubuntu <ubuntu@ip-172-31-33-87.ap-northeast-1.compute.internal>
2023-05-08 19:12:41 +0000

    update with initial scripts

--- 4_MGG_API.py
+++ 4_MGG_API.py
@@ -6,7 +6,7 @@ os.system("mv *.log logs/")
 num_gpus = [8]
 
 for gpu in num_gpus:
-    os.system("./bench_MGG_API.py {0} {1} > MGG_{0}GPU_API_Thread.log".format(gpu, 0))
+    os.system("./bench_MGG_API.py {0} {1} | tee MGG_{0}GPU_API_Thread.log".format(gpu, 0))
     os.system("./analysis_MGG.py MGG_{0}GPU_API_Thread.log {0}".format(gpu))
     os.system("mv MGG_{0}GPU_API_Thread.log logs/".format(gpu))
 
